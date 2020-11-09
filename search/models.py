1430e1f4f879e12ee4395713c01f817dcea03fd1
learningnoobi <64244098+learningnoobi@users.noreply.github.com>
2020-11-10 02:29:56 +0545

    weather initial add

--- search/models.py
+++ search/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+   
--- search/urls.py
+++ search/urls.py
@@ -4,4 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', views.index , name = "homepage"),
+    path('weather-app/', views.weather,name="weather"),
+    path('delete/<city_name>/', views.delete_city, name='delete_city'),
  ]
